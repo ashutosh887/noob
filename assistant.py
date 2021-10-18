80ddb3943159e2099dc83859e83a735b5b3d188a
ashutosh887 <ashutoshj887@gmail.com>
2021-10-19 01:17:37 +0530

    Added 2 functionalities

--- assistant.py
+++ assistant.py
@@ -1,10 +1,11 @@
+import os
+import random
+import datetime
 import pyttsx3
 import speech_recognition as sr
 import wikipedia
-import random
-import os
 import webbrowser
-import datetime
+import smtplib
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty('voices')
@@ -51,6 +52,15 @@ def takeCommand():
         return "None"
     return query
 
+def sendEmail(to, content):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login('myemailID@gmail.com', "myPassword")
+    server.sendmail("myemailID@gmail.com", to, content)
+    server.close()
+
+
 if __name__ == "__main__":
     greet()
 
@@ -84,3 +94,20 @@ if __name__ == "__main__":
         elif 'the time' in query:
             time = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir the Time is {time}")
+
+        elif 'code' in query:
+            codePath = "C:\\Users\\asus\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+            speak("Opening Visual Studio Code")
+
+        elif 'email to' in query:
+            try:
+                speak("What is the message to be sent!")
+                content = takeCommand()
+                to = "xyz@gmail.com"
+                sendEmail(to, content)
+                speak("Email had been sent to ", to)
+
+            except Exception as e:
+                print(e)
+                speak("Sorry! Unable to send the Email...")
